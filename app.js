042fcbc8b88a5e587eadb9282286834c58f39006
Matheus Henrique <19matheushenrique03@gmail.com>
2024-02-02 16:34:24 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,39 +3,55 @@ const div = document.querySelector('.header')
 const h2 = document.createElement('h2')
 const correctAnswers = ['B','B','A','B','A']
 
-
-
-form.addEventListener('submit', event => {
+const handleSubmit = event => {
   event.preventDefault()
-  let score = 0
-  
-  const userAnswers = [
-    form.inputQuestion1.value,
-    form.inputQuestion2.value,
-    form.inputQuestion3.value,
-    form.inputQuestion4.value,
-    form.inputQuestion5.value,
-  ]
-  
-  userAnswers.forEach((answer,index) => {
-    if(answer === correctAnswers[index]){
-      score += 20
-    }
-  })
-
-  scrollTo(0,0)
-
   h2.classList.add('score')
   div.insertAdjacentElement('beforeend',h2)
-
+  let score = 0
   let counter = 0
 
-  const timer = setInterval(() => {
+  let userAnswers = [
+    form.inputQuestion1,
+    form.inputQuestion2,
+    form.inputQuestion3,
+    form.inputQuestion4,
+    form.inputQuestion5,
+  ]
+  
+  const addScore = (answer,index) => {
+    if(answer.value === correctAnswers[index]){
+      score += 20
+    }
+  }
+
+  const addBackground = color => h2.style.background = color
+ 
+  const funcInterval = () => {
     if(counter === score){
       clearInterval(timer)
     }
+
+    if(counter >= 80){
+      addBackground('rgb(120, 241, 120)')
+    } else if(counter < 80 && counter >= 40){
+      addBackground('rgb(236, 236, 87)')
+    } else {
+      addBackground('rgb(243, 79, 79)')
+    }
+    
     h2.innerHTML= `Você acertou <span class='porcentage'>${counter}%</span> do quiz!`
     counter++
-  },10)
+  }
+  
+  userAnswers.forEach(addScore)
+  scrollTo(0,0)
+  const timer = setInterval(funcInterval,30)
 
-})
+  for(let i = 0;i < userAnswers.length;i++){
+    for(let x = 0;x < userAnswers[i].length;x++){
+      userAnswers[i][x].checked = false
+    }
+  }
+}
+
+form.addEventListener('submit', handleSubmit)
